53533db01c3ff0f2b7d3832affc2045d2b718df9
Jaz Hays <emersonjazhays@gmail.com>
2022-03-27 12:04:35 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(name='MECraker',
+      version='1.0',
+      description='Data collection tools for the Missouri Ethics Commission.',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      license="GNU AFFERO GPL 3.0",
+      author='Jaz Hays',
+      author_email='emersonjazhays@gmail.com',
+      url="https://github.com/RadJaz/MECraker",
+      packages=['mec', 'mec.report', 'mec.scraper', 'mec.watcher', 'mec.report.templates'],
+      package_data={'mec.report.templates': ['*.json']},
+      install_requires= ["requests", "PyMuPDF"]
+     )
